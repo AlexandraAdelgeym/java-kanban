9c252c05c9984e70782c5f64680e833895dcb5e4
Alexandra <adelgejm2003@yandex.ru>
2024-05-20 18:55:56 +0300

    sec ver

--- src/tracker/model/Epic.java
+++ src/tracker/model/Epic.java
@@ -2,76 +2,53 @@ package tracker.model;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task {
     private final List<Subtask> subtasks;
-    private Duration duration;
-    private LocalDateTime startTime;
     private LocalDateTime endTime;
 
     public Epic(String name, String description, List<Subtask> subtasks) {
         super(name, description, Status.NEW);
         this.subtasks = subtasks;
-        this.duration = Duration.ZERO;
+        this.setDuration(Duration.ZERO);
     }
 
     public List<Subtask> getSubtasks() {
         return subtasks;
     }
 
-    public void setDuration(Duration duration) {
-        this.duration = duration;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
+    public LocalDateTime getEndTime() {
+        return endTime;
     }
 
     public void setEndTime(LocalDateTime endTime) {
         this.endTime = endTime;
     }
 
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
     public void calculateDurationAndTimes() {
-        this.duration = subtasks.stream()
+        Duration totalDuration = subtasks.stream()
                 .map(Subtask::getDuration)
                 .reduce(Duration.ZERO, Duration::plus);
+        this.setDuration(totalDuration);
 
-        this.startTime = subtasks.stream()
+        LocalDateTime earliestStartTime = subtasks.stream()
                 .map(Subtask::getStartTime)
                 .min(LocalDateTime::compareTo)
                 .orElse(null);
+        this.setStartTime(earliestStartTime);
 
-        this.endTime = subtasks.stream()
+        LocalDateTime latestEndTime = subtasks.stream()
                 .map(Subtask::getEndTime)
                 .max(LocalDateTime::compareTo)
                 .orElse(null);
+        this.endTime = latestEndTime;
     }
 
     @Override
     public String toString() {
-        return "Epic{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", status=" + getStatus() +
-                ", subtasks=" + subtasks +
-                ", duration=" + duration.toMinutes() +
-                ", startTime=" + startTime +
-                ", endTime=" + endTime +
-                '}';
+        return String.format("Epic{id=%d, name='%s', description='%s', status=%s, subtasks=%s, duration=%s, startTime=%s, endTime=%s}",
+                getId(), getName(), getDescription(), getStatus(), subtasks, getDuration(), getStartTime(), endTime);
     }
 
     public void cleanSubtaskIds() {
